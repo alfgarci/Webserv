56326fbc00f749e01107ccc43b74202de02ad4dc
Vicente Garcia Andrade <vicgarci@c1r2s5.42madrid.com>
2024-08-20 19:46:07 +0200

    delete method enable in config

--- ServerInfo/store/verify_http_methods.cpp
+++ ServerInfo/store/verify_http_methods.cpp
@@ -12,9 +12,11 @@ bool	ServerInfo::verify_http_methods(list<t_server> &lst)
 	list<string>::iterator		it;
 	bool						get_flag;
 	bool						post_flag;
+	bool						delete_flag;
 
 	get_flag = false;
 	post_flag = false;
+	delete_flag = false;
 	server_it = lst.begin();
 	while (server_it != lst.end())
 	{
@@ -22,15 +24,15 @@ bool	ServerInfo::verify_http_methods(list<t_server> &lst)
 		while (route_it != server_it->routes.end())
 		{
 			if (route_it->accepted_http_methods.empty())
-				return (false);
+				return (true);
 			it = route_it->accepted_http_methods.begin();
 			while (it != route_it->accepted_http_methods.end())
 			{
-				cout << *it << endl;
 				if
 				(
 					*it != DEFAULT_METHOD_GET
 					&& *it != DEFAULT_METHOD_POST
+					&& *it != DEFAULT_METHOD_DELETE
 				)
 					return (false);
 				if (*it == DEFAULT_METHOD_GET && get_flag)
@@ -41,14 +43,20 @@ bool	ServerInfo::verify_http_methods(list<t_server> &lst)
 					return (false);
 				else if (*it == DEFAULT_METHOD_POST)
 					post_flag = true;
+				if (*it == DEFAULT_METHOD_DELETE && delete_flag)
+					return (false);
+				else if (*it == DEFAULT_METHOD_DELETE)
+					delete_flag = true;
 				it++;
 			}
 			get_flag = false;
 			post_flag = false;
+			delete_flag = false;
 			route_it++;
 		}
 		get_flag = false;
 		post_flag = false;
+		delete_flag = false;
 		server_it++;
 	}
 	return (true);
